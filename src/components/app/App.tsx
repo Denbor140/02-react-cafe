6b87ea32d056d113c78a160db208080eae68aa2d
Denys Boreiko <denbor140@gmail.com>
2026-01-12 13:54:49 +0200

    Fix import path for Notification component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,7 +4,7 @@ import type { Votes, VoteType } from "../../types/votes";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notification/Notification";
+import Notification from "../notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
