00f0a2857bf913d91c0549f76d51e4849c0edfd9
MAK <denbor140@gmail.com>
2026-01-12 14:25:50 +0200

    fix 12312

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import css from "./App.module.css";
 import { useState } from "react";
-import type { Votes, VoteType } from "../types/votes";
-import CafeInfo from "./CafeInfo/CafeInfo";
-import VoteOptions from "./VoteOptions/VoteOptions";
-import VoteStats from "./VoteStats/VoteStats";
-import Notification from "./notification/Notification";
+import type { Votes, VoteType } from "../../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
